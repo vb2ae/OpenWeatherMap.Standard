50e8c361fffd7f4b0279d38088ca6746672bf508
Ken Tucker <vb2ae@live.com>
2023-10-02 07:38:46 -0400

    fix codeql issues

--- OpenWeatherMap.Standard.Sample/Program.cs
+++ OpenWeatherMap.Standard.Sample/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using OpenWeatherMap.Standard.Enums;
+using OpenWeatherMap.Standard.Models;
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using OpenWeatherMap.Standard.Enums;
-using OpenWeatherMap.Standard.Models;
 
 namespace OpenWeatherMap.Standard.Sample
 {
@@ -11,7 +12,11 @@ namespace OpenWeatherMap.Standard.Sample
     {
         private static async Task Main(string[] args)
         {
-            var key = "USE_YOUR_KEY_PLEASE";
+            var config = new ConfigurationBuilder()
+                             .AddUserSecrets<Program>()
+                             .Build();
+            var key = config["Weather:ApiKey"];
+
             var current = new Current(key)
             {
                 Languages = Languages.German,
@@ -22,54 +27,46 @@ namespace OpenWeatherMap.Standard.Sample
             WeatherData data = null;
             ForecastData forecastData = null;
 
-            var getWeather = Task.Run(async () => { data = await current.GetWeatherDataByZipAsync("32927", "us"); });
-            getWeather.Wait();
+            data = await current.GetWeatherDataByZipAsync("32927", "us");
+
+
             Console.WriteLine(
                 $"[zip code]: current temperature in area zip-coded 32927 US is: {data.WeatherDayInfo.Temperature}");
 
-            var getWeatherCity = Task.Run(async () =>
-            {
-                data = await current.GetWeatherDataByCityNameAsync("berlin", "de");
-            });
-            getWeatherCity.Wait();
-            Console.WriteLine(
-                $"[city, country code]: current temperature in Berlin, Germany is: {data.WeatherDayInfo.Temperature}");
 
-            var getWeatherCityWOCountry = Task.Run(async () =>
-            {
-                data = await current.GetWeatherDataByCityNameAsync("baghdad");
-            });
-            getWeatherCityWOCountry.Wait();
+            data = await current.GetWeatherDataByCityNameAsync("berlin", "de");
+
+            Console.WriteLine(
+                    $"[city, country code]: current temperature in Berlin, Germany is: {data.WeatherDayInfo.Temperature}");
+
+
+            data = await current.GetWeatherDataByCityNameAsync("baghdad");
+
             Console.WriteLine($"[city]: current temperature in Baghdad is: {data.WeatherDayInfo.Temperature}");
 
-            var getWeatherCoords = Task.Run(async () =>
-            {
-                data = await current.GetWeatherDataByCoordinatesAsync(-33.865143, 151.209900);
-            });
-            getWeatherCoords.Wait();
+
+            data = await current.GetWeatherDataByCoordinatesAsync(-33.865143, 151.209900);
+
             Console.WriteLine($"[lat, lon]: current temperature in Sydney is: {data.WeatherDayInfo.Temperature}");
 
-            var saveIcon = Task.Run(async () =>
-            {
-                await using var fs = new FileStream($"{data?.Weathers[0]?.Icon}.png", FileMode.Create);
-                await fs.WriteAsync(data?.Weathers[0]?.IconData);
-            });
-            saveIcon.Wait();
-            
-            var getForecastWeather = Task.Run(async () =>
-            {
-                forecastData = await current.GetForecastDataByCityNameAsync("Schnelsen");
-            });
-            getForecastWeather.Wait();
+
+            await using var fs = new FileStream($"{data?.Weathers[0]?.Icon}.png", FileMode.Create);
+            await fs.WriteAsync(data?.Weathers[0]?.IconData);
+
+
+
+            forecastData = await current.GetForecastDataByCityNameAsync("Schnelsen");
+
             foreach (var weatherDayInfo in forecastData.WeatherData.Select(a => new
-                     {
-                         a.WeatherDayInfo,
-                         a.AcquisitionDateTime
-                     }))
+            {
+                a.WeatherDayInfo,
+                a.AcquisitionDateTime
+            }))
                 Console.WriteLine(
                     $"[forecast]: Forecast for Schnelsen, Germany at {weatherDayInfo.AcquisitionDateTime}, maximum temp: {weatherDayInfo.WeatherDayInfo.MaximumTemperature}, minimum temp: {weatherDayInfo.WeatherDayInfo.MinimumTemperature}");
 
             var forecastDataSync = current.GetForecastDataByCityName("schnelsen");
+            Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(forecastDataSync));
             var geolocations = await current.GetGeoLocationAsync("titusville", "fl", "usa");
             foreach (var location in geolocations)
             {
